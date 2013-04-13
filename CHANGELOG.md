de0f602e265de793432592b2c2763da9487112f5
Tim Jarratt <tjarratt@gmail.com>
2013-04-13 13:34:57 -0700

    Refactor savon-multipart for Savon2
    Needs tests but the interface between savon-multipart and savon just became a
    whole lot smaller.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,8 @@
 
 * Initial version. Adds multipart support (SOAP with Attachments) to Savon v0.9.7.
   Please test and provide feedback so we can merge this into Savon proper.
+
+## 2.0.0 (UNRELEASED)
+* Rewrite of savon-multipart to work with Savon 2.0+
+  This includes all of the existing functionality in savon-multipart
+  for savon1, but in a way that will be easier to maintain.
---
+++ lib/savon/multipart/response.rb
@@ -0,0 +1,54 @@
+require 'mail'
+
+module Savon
+  module Multipart
+    class Response < Savon::Response
+      attr_reader :parts
+
+      def initialize(*args)
+        @parts = []
+        super
+      end
+
+      def attachments
+        if multipart?
+          parse_body unless @has_parsed_body
+          @parts.attachments
+        else
+          []
+        end
+      end
+
+      def to_xml
+        if multipart?
+          parse_body unless @has_parsed_body
+          @parts.first.body
+        else
+          super
+        end
+      end
+
+      private
+      def multipart?
+        http.headers['Content-Type'] =~ /^multipart/
+      end
+
+      def boundary
+        return unless multipart?
+        @boundary ||= Mail::Field.new('Content-Type', http.headers['Content-Type']).parameters['boundary']
+      end
+
+      def parse_body
+        unless multipart?
+          super
+        else
+          @parts = Mail::Part.new(
+            :headers => http.headers,
+            :body => http.body
+          ).body.split!(boundary).parts
+          @has_parsed_body = true
+        end
+      end
+    end
+  end
+end
