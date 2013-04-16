5da3a51af5f02c8c11e6258bd9aa4b3eaed41b69
Tim Jarratt <tjarratt@gmail.com>
2013-04-16 16:52:03 -0700

    Update rspec tests
    Nothing crazy, just changed how we create the response

--- spec/savon/soap/response_spec.rb
+++ spec/savon/soap/response_spec.rb
@@ -1,6 +1,6 @@
 require 'savon-multipart'
 
-describe Savon::SOAP::Response do
+describe Savon::Multipart::Response do
 
   before do
     @header = { "Content-Type" => 'multipart/related; boundary="--==_mimepart_4d416ae62fd32_201a8043814c4724"; charset=UTF-8; type="text/xml"' }
@@ -22,11 +22,36 @@ describe Savon::SOAP::Response do
     response.attachments.size.should == 1
   end
 
+  it "parses soap messages without attachments too" do
+    header = { 'Content-Type' => 'text/html; charset=utf-8'}
+    body = File.read(File.expand_path('../../../fixtures/response/not_multipart.txt', __FILE__))
+    response = soap_response :headers => header, :body => body
+
+    response.to_xml.chomp.should == '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:wsdl="http://www.3gpp.org/ftp/Specs/archive/23_series/23.140/schema/REL-5-MM7-1-2" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Header><ns1:TransactionID soapenv:actor="" soapenv:mustUnderstand="1" xsi:type="xsd:string" xmlns:ns1="http://www.3gpp.org/ftp/Specs/archive/23_series/23.140/schema/REL-5-MM7-1-2">2011012713535811111111111</ns1:TransactionID></soapenv:Header><soapenv:Body><SubmitReq xmlns="http://www.3gpp.org/ftp/Specs/archive/23_series/23.140/schema/REL-5-MM7-1-2"><MM7Version>5.3.0</MM7Version><SenderIdentification><VASPID>messaging</VASPID><VASID>ADM</VASID><SenderAddress><ShortCode>1111</ShortCode></SenderAddress></SenderIdentification><Recipients><To><Number>11111111111</Number></To></Recipients><ServiceCode>1</ServiceCode><MessageClass>Personal</MessageClass><ExpiryDate>2011-01-28T13:53:58Z</ExpiryDate><DeliveryReport>false</DeliveryReport><ReadReply>false</ReadReply><Priority>Normal</Priority><Subject>Test MMS via Savon</Subject><ChargedParty>Sender</ChargedParty><Content href="cid:attachment_1" allowAdaptations="true"/></SubmitReq></soapenv:Body></soapenv:Envelope>'
+    response.parts.size.should == 0
+    response.attachments.size.should == 0
+  end
+
+  it "only parses the SOAP body once" do
+    response = soap_response :headers => @header, :body => @body
+    response.to_xml
+
+    counter = 0
+    subbed_parse_body = lambda { counter += 1 }
+    response.send(:define_singleton_method, :parse_body, subbed_parse_body)
+    5.times { response.attachments }
+
+    counter.should == 0
+  end
+
   def soap_response(options = {})
     defaults = { :code => 200, :headers => {}, :body => "" }
     response = defaults.merge options
+    globals = {:multipart => true}
+    locals = {}
+    http = HTTPI::Response.new(response[:code], response[:headers], response[:body])
 
-    Savon::SOAP::Response.new Savon.config, HTTPI::Response.new(response[:code], response[:headers], response[:body])
-  end
+    Savon::Multipart::Response.new(http, globals, locals)
+end
 
 end
