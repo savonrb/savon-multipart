076a1af97c8274cf734b9f8f0c81247d360daad1
Anton Fedorov <datacompboy@call2ru.com>
2014-03-03 14:20:47 +0700

    Fix multipart detection

--- lib/savon/multipart/response.rb
+++ lib/savon/multipart/response.rb
@@ -30,7 +30,7 @@ module Savon
 
       private
       def multipart?
-        http.headers['content-type'] =~ /^multipart/
+        !(http.headers['content-type'] =~ /^multipart/).nil?
       end
 
       def boundary
