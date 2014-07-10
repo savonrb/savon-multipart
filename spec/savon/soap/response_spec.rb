92cc1ff2f9e3e27b4ebb0deba6013534116013b0
David Wilkie <dwilkie@gmail.com>
2014-07-10 11:22:12 +0700

    add test case for SOAPFault

--- spec/savon/soap/response_spec.rb
+++ spec/savon/soap/response_spec.rb
@@ -11,6 +11,7 @@ describe Savon::Multipart::Response do
     response = defaults.merge options
     globals = {
       :multipart => true,
+      :raise_errors => true,
       :convert_response_tags_to  => lambda { |tag| tag.snakecase.to_sym}
     }
     http = HTTPI::Response.new(response[:code], response[:headers], response[:body])
@@ -60,6 +61,14 @@ describe Savon::Multipart::Response do
       expect(response.xml.chomp).to eq('<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:wsdl="http://www.3gpp.org/ftp/Specs/archive/23_series/23.140/schema/REL-5-MM7-1-2" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Header><ns1:TransactionID soapenv:actor="" soapenv:mustUnderstand="1" xsi:type="xsd:string" xmlns:ns1="http://www.3gpp.org/ftp/Specs/archive/23_series/23.140/schema/REL-5-MM7-1-2">2011012713535811111111111</ns1:TransactionID></soapenv:Header><soapenv:Body><SubmitReq xmlns="http://www.3gpp.org/ftp/Specs/archive/23_series/23.140/schema/REL-5-MM7-1-2"><MM7Version>5.3.0</MM7Version><SenderIdentification><VASPID>messaging</VASPID><VASID>ADM</VASID><SenderAddress><ShortCode>1111</ShortCode></SenderAddress></SenderIdentification><Recipients><To><Number>11111111111</Number></To></Recipients><ServiceCode>1</ServiceCode><MessageClass>Personal</MessageClass><ExpiryDate>2011-01-28T13:53:58Z</ExpiryDate><DeliveryReport>false</DeliveryReport><ReadReply>false</ReadReply><Priority>Normal</Priority><Subject>Test MMS via Savon</Subject><ChargedParty>Sender</ChargedParty><Content href="cid:attachment_1" allowAdaptations="true"/></SubmitReq></soapenv:Body></soapenv:Envelope>')
       expect(response.parts.size).to eq(0)
       expect(response.attachments.size).to eq(0)
+    end
+  end
+
+  context "soap errors" do
+    let(:path) { File.expand_path('../../../fixtures/response/soap_fault.txt', __FILE__) }
+
+    it "handles them correctly" do
+      expect { response }.to raise_error(Savon::SOAPFault, /The service cannot be found/)
     end
   end
 end
