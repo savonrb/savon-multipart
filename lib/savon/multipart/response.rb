a8f77134372642d45f60be653b839c9c047b1416
Peter Cai <222655+pcai@users.noreply.github.com>
2026-09-10 11:56:39 -0400

    Fix lint offenses and enforce lint in CI (#35)
    * Fix all 139 RuboCop offenses (safe auto-correct + hand fixes)
    * Exclude Gemspec/RequiredRubyVersion: gemspec floors are
      intentionally low; raising them is a separate breaking-change decision
    * Exclude lib/savon-multipart.rb from Naming/FileName: the entry point
      matches the gem name, renaming would break require "savon-multipart"
    * Add rubygems_mfa_required metadata (required by Gemspec/RequireMFA)
    * Move spec to spec/savon/multipart/ to match the described class
    * Refactor Response#xml / #parse_xop to satisfy Metrics cops
    * Remove continue-on-error from the lint job so lint is required

--- lib/savon/multipart/response.rb
+++ lib/savon/multipart/response.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 require 'mail'
 require 'cgi'
 
 module Savon
   module Multipart
+    # Savon::Response subclass that understands multipart/related SOAP
+    # responses, exposing MIME parts, attachments and XOP content.
     class Response < Savon::Response
       attr_reader :parts
 
@@ -21,20 +25,23 @@ module Savon
       end
 
       def xml
-        if multipart?
-          parse_body unless @has_parsed_body
-          if xop?
-            parse_xop unless @has_parsed_xop
-            @xop_body
-          else
-            @parts.first.body.to_s
-          end
-        else
-          super
-        end
+        return super unless multipart?
+
+        parse_body unless @has_parsed_body
+        multipart_xml
       end
 
       private
+
+      def multipart_xml
+        if xop?
+          parse_xop unless @has_parsed_xop
+          @xop_body
+        else
+          @parts.first.body.to_s
+        end
+      end
+
       def multipart?
         !(http.headers['content-type'] =~ /^multipart/im).nil?
       end
@@ -42,7 +49,7 @@ module Savon
       def xop?
         if multipart?
           parse_body unless @has_parsed_body
-          !(@parts.first.header['content-type'].to_s =~ /^application\/xop\+xml/i).nil?
+          !(@parts.first.header['content-type'].to_s =~ %r{^application/xop\+xml}i).nil?
         else
           false
         end
@@ -50,34 +57,42 @@ module Savon
 
       def boundary
         return unless multipart?
+
         @boundary ||= Mail::Field.new('content-type', http.headers['content-type']).parameters['boundary']
       end
 
       def parse_body
         @parts = Mail::Part.new(
-          :headers => http.headers,
-          :body => http.body
+          headers: http.headers,
+          body: http.body
         ).body.split!(boundary).parts
         @has_parsed_body = true
       end
 
       def parse_xop
-        xml = @parts.first.body.to_s
-        parsed = Nokogiri.XML(xml)
-        xop_elements = parsed.xpath('//xop:Include', xop: "http://www.w3.org/2004/08/xop/include")
-        if xop_elements.count == 0
+        parsed = Nokogiri.XML(@parts.first.body.to_s)
+        xop_elements = parsed.xpath('//xop:Include', xop: 'http://www.w3.org/2004/08/xop/include')
+
+        if xop_elements.empty?
           @xop_body = @parts.first.body.to_s
-          @has_parsed_xop = true
-          return
+        else
+          replace_xop_includes(xop_elements)
+          @xop_body = parsed.to_s
         end
+        @has_parsed_xop = true
+      end
+
+      def replace_xop_includes(xop_elements)
         xop_elements.each do |xop_element|
           href = xop_element.attributes['href'].to_s
-          cid = CGI.unescape(href[4..-1].to_s)
-          data = @parts.find { |p| p.header['content-id'].to_s == "<#{cid}>" }.body.to_s
+          cid = CGI.unescape(href[4..])
+          data = find_part_by_cid(cid).body.to_s
           xop_element.parent.content = Base64.encode64(data).chomp
         end
-        @xop_body = parsed.to_s
-        @has_parsed_xop = true
+      end
+
+      def find_part_by_cid(cid)
+        @parts.find { |part| part.header['content-id'].to_s == "<#{cid}>" }
       end
     end
   end
